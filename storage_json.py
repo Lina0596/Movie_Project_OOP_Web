324b77359f3fd8abeaf9da1c67f78b6a15bd7174
Lina0596 <lina.dahlhaus@icloud.com>
2025-01-30 16:10:35 +0100

    change class StorageJson

--- storage_json.py
+++ storage_json.py
@@ -1,21 +1,40 @@
 from istorage import IStorage
 import json
+import os
 
 
 class StorageJson(IStorage):
     def __init__(self, file_path):
         self.file_path = file_path
-        self.movies = {}
-        json_movies = json.dumps(self.movies, indent=4)
-        with open (self.file_path, "w") as movie_data:
-            movie_data.write(json_movies)
+        if os.path.exists(self.file_path):
+            with open(self.file_path, "r") as movie_data:
+                movies = json.loads(movie_data.read())
+            json_movies = json.dumps(movies, indent=4)
+            with open(self.file_path, "w") as movie_data:
+                movie_data.write(json_movies)
+        else:
+            self.movies = {
+                "The Shawshank Redemption": {"year": 1994, "rating": 9.5},
+                "Pulp Fiction": {"year": 1994, "rating": 8.8},
+                "The Room": {"year": 2003, "rating": 3.6},
+                "The Godfather": {"year": 1972, "rating": 9.2},
+                "The Godfather Part II": {"year": 1974, "rating": 9.0},
+                "The Dark Knight": {"year": 2008, "rating": 9.0},
+                "12 Angry Men": {"year": 1957, "rating": 8.9},
+                "Everything Everywhere All At Once": {"year": 2022, "rating": 8.9},
+                "Forrest Gump": {"year": 1994, "rating": 8.8},
+                "Star Wars Episode V": {"year": 1980, "rating": 8.7}
+            }
+            json_movies = json.dumps(self.movies, indent=4)
+            with open(self.file_path, "w") as movie_data:
+                movie_data.write(json_movies)
+
 
     def list_movies(self):
         """
         Returns a dictionary of dictionaries that
         contains the movies information in the database.
         """
-
         with open(self.file_path, "r") as movie_data:
             movies = json.loads(movie_data.read())
         return movies
