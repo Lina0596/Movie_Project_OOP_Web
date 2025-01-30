1f2158e9a1cf2fb5cf303bc64f8500f48abf3534
Lina0596 <lina.dahlhaus@icloud.com>
2025-01-30 16:11:39 +0100

    change main to test class StorageCsv

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from movie_app import MovieApp
 from storage_json import StorageJson
+from storage_csv import StorageCsv
 
 
 def main():
-    storage = StorageJson('movies.json')
+    storage = StorageCsv('movies.csv')
     movie_app = MovieApp(storage)
     movie_app.run()
 
